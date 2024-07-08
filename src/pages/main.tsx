dfd56680caaaf99f79609bec11200584937678c5
ilyestearti5 <ilyestearti5@gmail.com>
2024-07-09 00:43:24 +0100

    Done

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,4 @@
 import "pdfmake/build/vfs_fonts";
 import { startApplication } from "../index";
 import { App } from "@/Components/App/Main/App";
-import { BrowserRouter } from "react-router-dom";
-startApplication(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-);
+startApplication(<App />);
